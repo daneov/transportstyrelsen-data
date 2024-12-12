6895559abcf7802815e6cd700e76919e480c723d
Daneo Van Overloop <7900887+daneov@users.noreply.github.com>
2024-12-12 09:22:26 +0100

    Use dedicated entities to group code logic
    This improves readability and maintainability. It also was a good test
    case to try out some of the techniques in the "Refactoring" book.
    
    As I'm not familiar with the Python setup for TDD and this script is
    quite managable, I went at it without focussing too much on it unit
    tests at this time.

--- scrape.py
+++ scrape.py
@@ -1,81 +1,80 @@
-import requests
-from datetime import datetime
-from zoneinfo import ZoneInfo
-import sys
-import json
-import csv
+from dataclasses import dataclass
+from datetime import datetime, timezone
 from lxml import html
+from zoneinfo import ZoneInfo
+import csv
+import json
+import requests
+import sys
 
-def scrape_website(url):
-    try:
+@dataclass
+class CaseDate:
+    year: str
+    month: str
+    day: str
+    timezone: ZoneInfo
+
+    def to_iso8601(self):
+        return self.__as_datetime().strftime("%Y-%m-%d")
+
+    def processing_time(self, since):
+        delta_days = (since - self.__as_datetime()).days
+        proccessing_time_in_weeks = delta_days / 7 # Difference in weeks
+
+        return proccessing_time_in_weeks
+
+    def __as_datetime(self):
+        return datetime(int(self.year), int(self.month), int(self.day), tzinfo=self.timezone)
+
+class API:
+    def get(self, url):
         # Fetch the page content
         response = requests.get(url, timeout=0.5)
-        if response.status_code != 200:
-            return {
-                "status": "error",
-                "error_message": f"Failed to fetch page: HTTP {response.status_code}"
-            }
+        response.raise_for_status()
 
-        year, month, day = extract_date(response.text)
-        combined, case_date_iso, today, weeks_waiting_period = calculate_fields(year, month, day)
+        return response.text
 
-        return {
-            "status": "success",
-            "today": today,
-            "case_date_iso": case_date_iso,
-            "waiting_period": weeks_waiting_period
-        }
-    except ValueError as e:
-        return {
-            "status": "error",
-            "error_message": f"An exception occurred: {str(e)}"
-        }
-
-def extract_date(content):
-    tree = html.fromstring(content)
-    
-    # Use XPath to extract the day, month name, and year
-    day_month = tree.xpath('normalize-space(//*[@id="page-alert-block"]/div/div/p/strong[1])')
-    year = tree.xpath('normalize-space(//*[@id="page-alert-block"]/div/div/p/strong[2])')
-    
-    if not day_month or not year:
-        raise ValueError("Could not find the expected elements on the page.")
-
-    day, month_name = day_month.split()
-
-    return year.strip(), map_month(month_name.strip()), day.strip()
-
-def map_month(original_month_name):
-    month_map = {
-        "januari": "01", "februari": "02", "mars": "03", "april": "04",
-        "maj": "05", "juni": "06", "juli": "07", "augusti": "08",
-        "september": "09", "oktober": "10", "november": "11", "december": "12"
+class SiteScraper:
+    __month_map = {
+        "januari": "01",
+        "februari": "02",
+        "mars": "03",
+        "april": "04",
+        "maj": "05",
+        "juni": "06",
+        "juli": "07",
+        "augusti": "08",
+        "september": "09",
+        "oktober": "10",
+        "november": "11",
+        "december": "12"
     }
-    month = month_map.get(original_month_name.lower())
-    
-    if not month:
-        raise ValueError(f"Unknown month name: {original_month_name}")
 
-    return month
-    
-def calculate_fields(year, month, day):
-    # Format date to ISO8601 format
-    stockholm_timezone = ZoneInfo("Europe/Stockholm")
-    case_date = datetime(int(year), int(month), int(day), tzinfo=stockholm_timezone)
-    case_date_iso = case_date.strftime("%Y-%m-%d")
-    
-    # Get today's date and time in ISO8601 format
-    today = datetime.now(tz=stockholm_timezone)
-    # Calculate the difference in days and weeks
-    delta_days = (today - case_date).days
-    delta_weeks = delta_days / 7
-    
-    return case_date.strftime('%d %B'), case_date_iso, today.isoformat(), delta_weeks
+    def extract_date(self, data):
+        tree = html.fromstring(data)
 
-def append_to_csv(file_path, data):
+        # Use XPath to extract the day, month name, and year
+        day_and_month = tree.xpath('normalize-space(//*[@id="page-alert-block"]/div/div/p/strong[1])')
+        year = tree.xpath('normalize-space(//*[@id="page-alert-block"]/div/div/p/strong[2])')
+
+        if not day_and_month or not year:
+            raise ValueError("Could not find the expected elements on the page.")
+
+        day, month_name = day_and_month.split()
+
+        return year.strip(), self.__map_month(month_name.strip()), day.strip()
+
+    def __map_month(self, original_month_name):
+        month = self.__month_map.get(original_month_name.lower())
+
+        if not month:
+            raise ValueError(f"Unknown month name: {original_month_name}")
+
+        return month
+
+def append_to_csv(file_path, today, case_date, processing_time):
     # Specify the header
     headers = ["Date", "Evaluating cases", "Waiting period"]
-    # Check if the file exists to write the header
     try:
         with open(file_path, 'a', newline='', encoding='utf-8') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=headers, dialect='unix')
@@ -84,21 +83,29 @@ def append_to_csv(file_path, data):
                 writer.writeheader()
 
             writer.writerow({
-                "Date": data["today"],
-                "Evaluating cases": data["case_date_iso"],
-                "Waiting period": round(data["waiting_period"], 2)
+                "Date": today,
+                "Evaluating cases": case_date,
+                "Waiting period": round(processing_time, 2)
             })
     except Exception as e:
         print(f"Error writing to CSV: {e}")
 
-
+source_timezone = ZoneInfo("Europe/Stockholm")
 url = "https://www.transportstyrelsen.se/sv/vagtrafik/fordon/aga-kopa-eller-salja-fordon/import-och-export-av-fordon/fordonsimport-och-ursprungskontroll/"
-result = scrape_website(url)
+csv_file_path = "transportstyrelsen_data.csv"
 
-# Return result
-if result["status"] == "success":
-    csv_file_path = "transportstyrelsen_data.csv"
-    append_to_csv(csv_file_path, result)
-    sys.stdout.write("Written to disk")
-else:
-    sys.stdout.write(f"Error: {result['error_message']}")
+def main():
+    api = API()
+    scraper = SiteScraper()
+    now = datetime.now(timezone.utc)
+
+    try:
+        case_date = CaseDate(*scraper.extract_date(api.get(url)), timezone=source_timezone)
+        append_to_csv(csv_file_path, now, case_date.to_iso8601(), case_date.processing_time(since=now))
+        print("Written to disk")
+    except ValueError as e:
+        print(f"Error: {e}", file=sys.stdout)
+        sys.exit(64)
+
+if __name__ == "__main__":
+    main()
